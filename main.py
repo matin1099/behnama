13ac6bea6fe07c20e70178ff4f460a0cff51b1fb
matin1099 <matin1378718@gmail.com>
2024-06-25 22:26:51 +0330

    modify config.json& documenting scrap.

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from files.cfg import Cfg
 from files.scrap import scrap
 from files.spotter import spotter
 
-
+"""TODO
+    make scrap check if link changed.(perv link in json)
+    returning name of new image for spotter
+    making spotter remain prev one
+    making old pics go to another dir
+    making cfg json file to write old names
+"""
 config = Cfg().read_cfg()
 tehran_pakhsh, ahora, yaran = config[0]["tehranPakhsh"], config[0]["ahora"], config[0]["yaran"]
 header = config[1]
